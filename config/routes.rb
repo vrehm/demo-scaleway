695ed525a117c0c8f3047f7ab01ba32dcad03d88
Vincent REHM <vincentrehm@yahoo.fr>
2020-05-16 14:56:30 +0200

    basic redis and sidekiq setup with no user NOT RECOMMENDED for real projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Sidekiq Web UI, only for admins.
+  require "sidekiq/web"
+  # authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  # end  
 end
