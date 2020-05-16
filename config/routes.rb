857264f4b66a6773f7eb07ec266af8ab838955a5
Vincent REHM <vincentrehm@yahoo.fr>
2020-05-16 15:12:40 +0200

    polishing production setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/check.txt', to: proc {[200, {}, ['it_works']]}
+
   # Sidekiq Web UI, only for admins.
   require "sidekiq/web"
   # authenticate :user, lambda { |u| u.admin } do
